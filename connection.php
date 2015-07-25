c9d0b5d45b0b8a752eeed9eece794c8e82c36093
ahmarmahmood <ahmar.aimy@live.com>
2015-07-26 02:23:25 +0500

    Employee Crud
    Created Employee insertion files

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "senhoitc_mis";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+?> 
