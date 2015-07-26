b51025f4d316872c167111f0ae35f29ba7afb46d
ahmarmahmood <ahmar.aimy@live.com>
2015-07-26 12:51:29 +0500

    Side Bar Height Fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,15 @@
 var sidebar = $('.page-sidebar');
 $(window).scroll(function(event) {
 	console.log("Scrolling");
-	var scrolled = $(window).scrollTop() + $(window).height() - 30;
+	var scrolled = $(window).scrollTop() + $(window).height();
 	var footerPosition = $('footer').position().top;
 	if (scrolled > footerPosition) {
 		sidebar.css('position', 'absolute');
+		sidebar.css('height', '100%');
 	}
 	else {
 		console.log('Less than');
 		sidebar.css('position', 'fixed');
+		sidebar.css('height', 'auto');
 	}
 });
